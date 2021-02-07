3527e1b1bf6f8aea0e448064375a5f08f8a7f13c
mehedihasanshipon <siponmahmud112255@gmail.com>
2021-02-07 13:34:35 +0600

    Assignment 5 completed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function for load data from theMealDb api Search meal by name
 const getFoodList = () => {
   let searchInputTxt = document.getElementById("search-input").value;
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
@@ -5,12 +6,14 @@ const getFoodList = () => {
     .then((data) => {
         displayFood(data.meals);
     });
+    document.getElementById("food-container").innerHTML = "";
 };
 
+// Function for display food list in UI
 const displayFood = (foods) =>{
+    let searchInputTxt = document.getElementById("search-input").value;
     let foodContainer = document.getElementById("food-container");
-    // let foods = data.meals;
-    if(foods === null || foods.length <0){
+    if(foods === null || searchInputTxt ==""){
       const foodDiv = document.createElement("div");
       const foodInfo = `
           <h3 class="nothing"> Sorry, we didn't find any food!,Please search again.</h3>
@@ -19,13 +22,12 @@ const displayFood = (foods) =>{
       foodContainer.appendChild(foodDiv);
     }else{
       foods.forEach((food) => {
-          // console.log(food);
           const foodDiv = document.createElement("div");
           foodDiv.className = "col-md-4 my-3 foodDiv";
           const foodInfo = `
-                  <div onclick="displayCountryDetail('${food.idMeal}')" class="card" style="width: 18rem;">
-                      <img  class="card-img-top" src="${food.strMealThumb}" alt="Card image cap">
-                      <div class="card-body">
+                  <div onclick="displayCountryDetail('${food.idMeal}')" class="card card-custom" style="width: 18rem;">
+                      <img class="card-img-top" src="${food.strMealThumb}" alt="Card image cap">
+                      <div class="card-body card-body-custom">
                           <p class="card-text"><h4>${food.strMeal}</h4></p>
                       </div>
                   </div>
@@ -35,8 +37,10 @@ const displayFood = (foods) =>{
         });
     }
     document.getElementById("search-input").value = "";
+    
 }
 
+// Function for load data from theMealDb api Search meal by id
 const displayCountryDetail = foodDetails => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodDetails}`
     fetch(url)
@@ -46,6 +50,7 @@ const displayCountryDetail = foodDetails => {
     });
 }
 
+// Function for display single food details including Ingredients in UI
 const getFoodInfo = food => {
     const foodDetail = document.getElementById('food-details');
     const ingredients = [];
